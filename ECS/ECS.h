f8651cddde7c00072f98bc3aba7b48cf69a6a41d
181847 <753090084@qq.com>
2017-12-14 15:02:44 +0800

    添加、修改注释

--- ECS/ECS.h
+++ ECS/ECS.h
@@ -7,21 +7,21 @@
 #pragma comment(lib, "ECS.lib")
 namespace ECS
 {
-
+	// global logger
 extern log4cplus::Logger gECSLogger;
-static std::string gLoggerLayout = "%d{%m/%d/%y %H:%M:%S} -%m[%l]%n";
-// the ECS support up to {gMaxComponentTypeCount} different componentType.
-static const size_t gMaxComponentTypeCount = 32;
+static const std::string gLoggerLayout		= "%d{%m/%d/%y %H:%M:%S} -%m[%l]%n";
+	// the ECS support up to {gMaxComponentTypeCount} different componentType.
+static const size_t gMaxComponentTypeCount	= 32;
 
-// seral key type in the ECS.
+	// seral key type in the ECS.
 using EntityID			= size_t;
 using ComponentTypeID	= size_t;
 using ComponentMask		= std::bitset<gMaxComponentTypeCount>;
 
-// starup the ECS
+	// starup the ECS
 int init();
 
-// initialize the gECSLogger.
+	// initialize the gECSLogger.
 int initLogger();
 
 }// namespace ECS
