f578a4414bacbe7b276f80763ac2570e96c94b2b
181847 <753090084@qq.com>
2017-12-16 23:21:16 +0800

    开始准备为Entity添加标记ComponentType的功能。
    预期使用模板的不定参数实现对多个ComponentType进行标记的功能。

--- ECS/EntityManager.h
+++ ECS/EntityManager.h
@@ -14,11 +14,23 @@ namespace ECS
 // you can access the component through ComponentManager with the EntityID.
 class EntityManager
 {
+	// the enum used to indicate the operationResult of maskComponentType,
+	// this enum type is major used in the recursion, 
+	// and we will combine each recursion result together.
+	enum MaskResult
+		: unsigned char
+	{ 
+		InvalidEntityID			= 1,	// 0001
+		Success					= 2,	// 0010 
+		RedundancyComponent		= 4,	// 0100
+		NoComponentType			= 8		// 1000
+	};
+
 	// be ware that the EntityID '0' means invalid Entity,
 	static const size_t maxEntityCount = 2048;
 
 	// the EntityFreeBlock is used to store the free entity ID,
-	// all the avaliable id are storey between the EntityFreBlock.start~end.
+	// all the avaliable id are stored between the EntityFreBlock.start~end.
 	// which entityID can be used .
 	typedef struct EntityFreeBlock
 	{
@@ -37,6 +49,17 @@ public:
 	bool destoryEntity(EntityID destoriedID);
 	// check if the checkID is a valid id;
 	bool isValid(EntityID checkID);
+
+	// add all the COMPONENT_TYPE's type mask to the entityID's mask, 
+	// this function can take multiple componentTypes and expand them
+	// through the recursion.
+	// recursion -> start
+	template<typename FIRST_COMPONENT_TYPE, typename ...REST_COMPONENT_TYPES>
+	MaskResult maskComponentType(EntityID entityID);
+	// recursion <- end
+	template<typename LAST_COMPONENT_TYPE>
+	MaskResult maskComponentType(EntityID entityID);
+	
 
 	size_t getSize() const;
 	size_t getUsedIDCount() const;
