b790f56da11d6bddc324d64271811cecb5246b5c
181847 <753090084@qq.com>
2017-12-17 15:56:35 +0800

    在Entitymanager中测试IDGenerator的新功能，测试EntityManager的mask组件类型的。能

--- TestEntityManager/TestEntityManager.cpp
+++ TestEntityManager/TestEntityManager.cpp
@@ -4,12 +4,12 @@
 #include <ECS\EntityManager.h>
 #include <MyTools\UnitTestModules.h>
 #include <MyTools\RandomTool.h>
+#include "TestComponentTypes.h"
 
 DECLARE_TEST_UNITS;
 
 #define DeclareEntityManager(managerName)\
 	ECS::EntityManager* managerName = ECS::EntityManager::getInstance()
-
 
 // get batch Entities,
 // return errors count, (if no error, return 0)
@@ -37,7 +37,9 @@ int newEntitis(
 
 // destory the batch Entitys£¬ and clear the idList.
 // return errors count, (if no error, return 0)
-int destoryEntities(ECS::EntityManager* eManager, std::vector<ECS::EntityID> * idList)
+int destoryEntities(
+	ECS::EntityManager* eManager, 
+	std::vector<ECS::EntityID> * idList)
 {
 	int error = 0;
 	for (ECS::EntityID id : *idList)
@@ -51,6 +53,7 @@ int destoryEntities(ECS::EntityManager* eManager, std::vector<ECS::EntityID> * i
 	return error;
 }
 
+// random allocate entityID, and random delete them.
 inline int testEntityManager(
 	ECS::EntityManager* pManager,
 	const size_t batchSize = 100,
@@ -107,7 +110,8 @@ namespace TestUnit
 
 void GetReady()
 {
-
+	ECS::ComponentIDGenerator::newID<IntComponent>();
+	ECS::ComponentIDGenerator::newID<FloatComponet>();
 }
 
 void AfterTest()
@@ -117,11 +121,40 @@ void AfterTest()
 
 void AddTestUnit()
 {
+	// test Unit always success test unit
+	{
 	TEST_UNIT_START("always success test unit")
 
 		return true;
 	TEST_UNIT_END;
+	}// test Unit always success test unit
 
+	// test Unit check IDGenerator's newID() and getID()
+	{
+		TEST_UNIT_START("check IDGenerator's newID() and getID()")
+			// a temp struct to create a series id.
+			struct TestIDGenStruct {};
+			using TestIDGen = TypeTool::IDGenerator<TestIDGenStruct>;
+
+			// first part, try first newID(), then getID().
+			errorLogger += NOT_EQ(1, TestIDGen::newID<int>());
+			errorLogger += NOT_EQ(2, TestIDGen::newID<float>());
+			errorLogger += NOT_EQ(1, TestIDGen::getID<int>());
+			errorLogger += NOT_EQ(2, TestIDGen::getID<float>());
+
+			// second part, first getID(), which should return zero.
+			// and newID(), which also return zero.
+			errorLogger += NOT_EQ(0, TestIDGen::getID<unsigned int>());
+			errorLogger += NOT_EQ(0, TestIDGen::getID<unsigned char>());
+			errorLogger += NOT_EQ(0, TestIDGen::newID<unsigned int>());
+			errorLogger += NOT_EQ(0, TestIDGen::newID<unsigned char>());
+			
+			return errorLogger.conclusion();
+		TEST_UNIT_END;
+	}
+
+	// test Unit create EntityManager
+	{
 	TEST_UNIT_START("creat EntityManager")
 		int error = 0;
 		/*ECS::EntityManager* eManager = 
@@ -140,7 +173,10 @@ void AddTestUnit()
 
 		return error == 0;
 	TEST_UNIT_END;
+	}// test Unit create EntityManager
 
+	// test Unit generat ID, and random destoried
+	{
 	TEST_UNIT_START("generat ID, and random destoried")
 		int error = 0;
 		DeclareEntityManager(eManager);
@@ -148,7 +184,10 @@ void AddTestUnit()
 		error += testEntityManager(eManager, 300, true, 30);
 		return error == 0;
 	TEST_UNIT_END;
+	}// test Unit generat ID, and random destoried
 
+	// test Unit test checkInvalid EntityID
+	{
 	TEST_UNIT_START("test checkInvalid EntityID")
 		DeclareEntityManager(eManager);
 
@@ -193,7 +232,7 @@ void AddTestUnit()
 		// now here is the main part
 		// iterate all the idList,
 		// check the EntityManager::isValid() is same as deleteFlagList.
-		for (int idIndex = 0; idIndex < idList.size(); ++idIndex)
+		for (size_t idIndex = 0; idIndex < idList.size(); ++idIndex)
 		{
 			errorLogger += NOT_EQ(
 				validFlagList[idIndex], 
@@ -202,7 +241,7 @@ void AddTestUnit()
 
 		// for clean the entityManager
 		// destory the rest entityID
-		for (int idIndex = 0; idIndex < idList.size(); ++idIndex)
+		for (size_t idIndex = 0; idIndex < idList.size(); ++idIndex)
 		{
 			if (validFlagList[idIndex])
 			{
@@ -212,6 +251,74 @@ void AddTestUnit()
 
 		return errorLogger.conclusion();
 	TEST_UNIT_END;
+	}// test Unit test checkInvalid EntityID
+
+	// test Unit test componentMask use EntityManager
+	{
+		TEST_UNIT_START("test componentMask use EntityManager")
+			DeclareEntityManager(eManager);
+
+			using MaskResultFlag = ECS::EntityManager::MaskResultFlag;
+			const size_t idCount = 5;
+			const size_t partOneCount = 2;
+			const size_t partTwoCount = idCount - partOneCount;
+
+			std::vector<ECS::EntityID> idList;
+			errorLogger += newEntitis(eManager, &idList, idCount);
+
+			RandomTool::RandomSet<size_t> randomIndices;
+			randomIndices.setSeed(1);
+			randomIndices.randomSequence(idCount);
+
+			// part one will mask two type of component[IntComponet, FloatComponent].
+			// part two will only mask with IntComponent.
+			for (size_t randomIndexPosition = 0;
+				randomIndexPosition < randomIndices.size();
+				++randomIndexPosition)
+			{
+				ECS::EntityManager::MaskResult result;
+				if (randomIndexPosition < partOneCount)
+				{
+					result = eManager->maskComponentType<IntComponent, FloatComponet>(idList[
+							randomIndices[randomIndexPosition]]);
+					errorLogger += NOT_EQ(result, MaskResultFlag::Success);
+				}
+				else
+				{
+					result = eManager->maskComponentType<IntComponent>(idList[
+						randomIndices[randomIndexPosition]]);
+					errorLogger += NOT_EQ(result, MaskResultFlag::Success);
+				}
+			}
+
+			// part one will append [IntComponent, CharComponent], 
+			//		which result should return Success | RedundancyComponent
+			// part two will only mask with FloatComponent,
+			//		which result should return Success.
+			for (size_t randomIndexPosition = 0;
+				randomIndexPosition < randomIndices.size();
+				++randomIndexPosition)
+			{
+				ECS::EntityManager::MaskResult result;
+				if (randomIndexPosition < partOneCount)
+				{
+					result = eManager->maskComponentType<IntComponent, CharComponent>(idList[
+						randomIndices[randomIndexPosition]]);
+					errorLogger += NOT_EQ(result, MaskResultFlag::Success | MaskResultFlag::RedundancyComponent);
+				}
+				else
+				{
+					result = eManager->maskComponentType<CharComponent>(idList[
+						randomIndices[randomIndexPosition]]);
+					errorLogger += NOT_EQ(result, MaskResultFlag::Success);
+				}
+			}
+
+			errorLogger += destoryEntities(eManager, &idList);
+			return errorLogger.conclusion();
+		TEST_UNIT_END;
+	}// test Unit test componentMask use EntityManager
+
 }// function void AddTestUnit()
 
 
