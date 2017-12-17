e0fc9d23d81384832cadbcb2e49d895c8066681c
181847 <753090084@qq.com>
2017-12-17 23:47:45 +0800

    增加一种EntityRange类，用来表示一个用来遍历的Entity集合。
    内部就是一个简单的保存了一个ComponentMask，用来区分不同的Entity类型，实现了begin()函数，end()还没有实现。

--- ECS/EntityManagerTool.h
+++ ECS/EntityManagerTool.h
@@ -18,60 +18,127 @@ public:
 	EntityFreeBlock* next;
 }EntityFreeBlock, *PEntityFreeBlock;
 
+
 // The iterator to traverse the desired entity.
 template<typename ...COMPONENT_TYPES>
 class EntityIter
 {
 private:
 	// target entity componetMask
-	const ComponentMask _desiredMask;
+	const ComponentMask&		_desiredMask;
 	// max entity Count.
-	const size_t _maxEntityCount;
+	const size_t&				_maxEntityCount;
 	// the pointer to the EntityManager's maskPool
-	const ComponentMask* const _maskPool;
+	const ComponentMask* const	_maskPool;
 	// the freeList which contain the invalid EntityIDs.
-	PEntityFreeBlock _pFreeList;
+	PEntityFreeBlock			_internalHead;
+	PEntityFreeBlock			_internalTail;
 	// next Entity
-	EntityID _nextID;
+	EntityID					_currID;
 
 public:
 	EntityIter(
+		const ComponentMask&		desiredMask,
 		const size_t&				maxEntityCount,
 		const ComponentMask* const	maskPool,
-		PEntityFreeBlock			pFreeListHead);
+		PEntityFreeBlock			internalHead,
+		PEntityFreeBlock			internalTail,
+		EntityID					startID = 0);
 
 	EntityID operator*();
 
 	EntityIter& operator ++();
 
-	bool operator != (const EntityIter<COMPONENT_TYPES...>& end) const;
+	bool operator != (const EntityIter<COMPONENT_TYPES...>& other) const;
+
+private:
+	EntityID getInternalStart();
+	EntityID getInternalEnd();
 };// class EntityIter
 
+template<typename ...COMPONENT_TYPES>
+class EntityRange
+{
+private:
+	// target entity componetMask
+	const ComponentMask				_desiredMask;
+	// max entity Count.
+	const size_t					_maxEntityCount;
+	// the pointer to the EntityManager's maskPool
+	const ComponentMask* const		_maskPool;
+	// the freeList which contain the invalid EntityIDs.
+	PEntityFreeBlock				_pFreeList;
+
+public:
+	EntityRange(
+		const size_t&				maxEntityCount, 
+		const ComponentMask* const	maskPool,
+		PEntityFreeBlock			pFreeList);
+
+	EntityIter<COMPONENT_TYPES...> begin();
+	
+	EntityIter<COMPONENT_TYPES...> end();
+};
 
 
 template<typename ...COMPONENT_TYPES>
 inline EntityIter<COMPONENT_TYPES...>::EntityIter(
-	const size_t & maxEntityCount,
-	const ComponentMask * const pMaskPool,
-	PEntityFreeBlock pFreeListHead)
+	const ComponentMask&				desiredMask,
+	const size_t &						maxEntityCount,
+	const ComponentMask * const			pMaskPool,
+	PEntityFreeBlock					internalHead,
+	PEntityFreeBlock					internalTail,
+	EntityID							startID)
 	:
-	_desiredMask(getComponentMask<COMPONENT_TYPES...>()),
+	_desiredMask(desiredMask),
 	_maxEntityCount(maxEntityCount),
 	_maskPool(pMaskPool),
-	_pFreeList(pFreeListHead)
+	_internalHead(internalHead),
+	_internalTail(internalTail),
+	_currID(startID)
 {
 
 }
 template<typename ...COMPONENT_TYPES>
 inline EntityID EntityIter<COMPONENT_TYPES...>::operator*()
 {
-	return _nextID;
+	return _currID;
 }
 template<typename ...COMPONENT_TYPES>
 inline EntityIter<COMPONENT_TYPES...> & EntityIter<COMPONENT_TYPES...>::operator++()
 {
+	// TODO:
+	static EntityID internalStart = _currID;
+	static EntityID internalEnd = getInternalEnd();
+	static bool findAnotherOne;
 
-	return *this;
+	findAnotherOne = false;
+	internalStart = _currID;
+
+	while (true)
+	{
+		for (++_currID; _currID <= internalEnd; ++_currID)
+		{
+			if ((_maskPool[_currID] & _desiredMask) == _desiredMask)
+			{
+				findAnotherOne = true;
+				break;
+			}
+		}
+
+		if (findAnotherOne || _internalTail == nullptr)
+		{
+			break;
+		}
+		else
+		{
+			_internalHead = _internalTail;
+			_internalTail = _internalTail->next;
+		}
+
+		internalStart = getInternalStart();
+		internalEnd = getInternalEnd();
+	}
 }
 
 template<typename ...COMPONENT_TYPES>
@@ -80,4 +147,82 @@ inline bool EntityIter<COMPONENT_TYPES...>::operator!=(const EntityIter<COMPONEN
 	return _nextID != 0;
 }
 
+template<typename ...COMPONENT_TYPES>
+inline EntityID EntityIter<...COMPONENT_TYPES>::getInternalStart()
+{
+	return _internalHead == nullptr ? 1 : _internalHead->end + 1;
+}
+
+template<typename ...COMPONENT_TYPES>
+inline EntityID EntityIter<...COMPONENT_TYPES>::getInternalEnd()
+{
+	return _internalTail == nullptr ? _maxEntityCount : _internalTail->start - 1;
+}
+
+template<typename ...COMPONENT_TYPES>
+inline 
+EntityRange<COMPONENT_TYPES...>::EntityRange(
+	const size_t&				maxEntityCount,
+	const ComponentMask* const	maskPool,
+	PEntityFreeBlock			pFreeList):
+	_desiredMask(getComponentMask<COMPONENT_TYPES...>()),
+	_maxEntityCount(maxEntityCount),
+	_maskPool(maskPool),
+	_pFreeList(pFreeList)
+{
+}
+template<typename ...COMPONENT_TYPES>
+inline EntityIter<COMPONENT_TYPES...> EntityRange<COMPONENT_TYPES...>::begin()
+{
+	// try to get the first desiredID
+	EntityID firstId;
+	EntityID internalStart, internalEnd;
+
+	PEntityFreeBlock internalHead, internalTail;
+
+	// next two pointer point to the two block inside which is the valid entities.
+	internalHead = (_pFreeList && _pFreeList->start == 1) ? _pFreeList : nullptr;
+	internalTail = (_pFreeList && _pFreeList->start == 1) ? _pFreeList->next : _pFreeList;
+
+	// get the start valid entityID
+	auto getInternalStart = [&internalHead]() ->EntityID {
+		return internalHead == nullptr ? 1 : internalHead->end + 1;
+	};
+
+	// get the end valid entityID
+	auto getInternalEnd = [&internalTail, this]() {
+		return internalTail == nullptr ? _maxEntityCount : internalTail->start - 1;
+	};
+
+	while (true)
+	{
+		internalStart = getInternalStart();
+		internalEnd = getInternalEnd();
+
+		for (firstId = internalStart; firstId <= internalEnd; ++firstId)
+		{
+			if ((_maskPool[firstId] & _desiredMask) == _desiredMask)
+			{
+				// a flag to break the outer while loop.
+				internalTail = nullptr;
+				break;
+			}
+		}
+
+		if (internalTail == nullptr)
+		{
+			break;
+		}
+		else
+		{
+			internalHead = internalTail;
+			internalTail = internalTail->next;
+		}
+	}
+
+	// have we find through all the entities?
+	firstId = firstId > _maxEntityCount ? 0 : firstId;
+	
+	return EntityIter<COMPONENT_TYPES...>(_desiredMask, _maxEntityCount, _maskPool, internalHead, internalTail, firstId);
+}
 }// namespace ECS
