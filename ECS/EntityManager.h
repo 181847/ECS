6166df245eec8fb8b50ff89a39c48efc8f1bcd5a
181847 <753090084@qq.com>
2018-01-16 22:41:00 +0800

    将EntityManager改为模板实现。
    使用一个模板参数Traits存储一些静态的配置信息，当前只配置一个最大可分配的Entity数量。

--- ECS/EntityManager.h
+++ ECS/EntityManager.h
@@ -13,20 +13,14 @@ namespace ECS
 // but the component is actually store in the ComponentManager,
 // the EntityManager didn't store it.
 // you can access the component through ComponentManager with the EntityID.
+// Traits:
+//		const MaxEntityCount;
+template<typename Traits>
 class EntityManager
 {
 public:
-	// the enum used to indicate the operationResult of maskComponentType,
-	// this enum type is major used in the recursion, 
-	// and we will combine each recursion result together.
-
-	// be aware that the EntityID '0' means invalid Entity,
-	static const size_t maxEntityCount = 2048;
-
-public:
-
 	// singleton
-	static EntityManager* getInstance();
+	static EntityManager<Traits>* getInstance();
 
 	// get a new Entity and return its ID.
 	EntityID	newEntity();
@@ -48,11 +42,11 @@ public:
 
 	// check if the entity have the component type.
 	template<typename ...COMPONENT_TYPES>
-	bool haveComponent(EntityID entityID);
+	bool		haveComponent(EntityID entityID);
 	
 	// get the iterator of the entity which should have the specific ComponentType.
 	template<typename ...COMPONENT_TYPES>
-	auto RangeEntities();
+	auto		RangeEntities();
 	
 	size_t getSize() const;
 	size_t getUsedIDCount() const;
@@ -62,15 +56,14 @@ private:
 
 	// add another one for entityID 0,
 	// which means a invalid ID.
-	ComponentMask		_maskPool[1 + maxEntityCount];
+	ComponentMask		_maskPool[1 + Traits::MaxEntityCount];
 	PEntityFreeBlock	_freeList;
 	size_t				_usedID;
 };
 
-
-
+template<typename Traits>
 template<typename COMPONENT_TYPE>
-inline MaskResult EntityManager::maskSingleComponentType(EntityID entityID)
+inline MaskResult EntityManager<Traits>::maskSingleComponentType(EntityID entityID)
 {
 	ASSERT(isValid(entityID));
 #ifdef _DEBUG
@@ -102,8 +95,9 @@ inline MaskResult EntityManager::maskSingleComponentType(EntityID entityID)
 	return MaskResultFlag::Success;
 }
 
+template<typename Traits>
 template<typename ...COMPONENT_TYPES>
-inline MaskResult EntityManager::maskComponentType(EntityID entityID)
+inline MaskResult EntityManager<Traits>::maskComponentType(EntityID entityID)
 {
 	ASSERT(isValid(entityID));
 #ifdef _DEBUG
@@ -156,8 +150,9 @@ inline MaskResult EntityManager::maskComponentType(EntityID entityID)
 	return result;
 }
 
+template<typename Traits>
 template<typename ...COMPONENT_TYPES>
-inline bool EntityManager::haveComponent(EntityID entityID)
+inline bool EntityManager<Traits>::haveComponent(EntityID entityID)
 {
 	ASSERT(isValid(entityID));
 	const ComponentMask combinedMask = ECS::getComponentMask<COMPONENT_TYPES...>();
@@ -172,10 +167,240 @@ inline bool EntityManager::haveComponent(EntityID entityID)
 	}
 }
 
+template<typename Traits>
 template<typename ...COMPONENT_TYPES>
-inline auto EntityManager::RangeEntities()
+inline auto EntityManager<Traits>::RangeEntities()
 {
-	return EntityRange<COMPONENT_TYPES...>(maxEntityCount, _maskPool, _freeList);
+	return EntityRange<COMPONENT_TYPES...>(Traits::MaxEntityCount, _maskPool, _freeList);
+}
+
+template<typename Traits>
+EntityManager<Traits> * EntityManager<Traits>::getInstance()
+{
+	static EntityManager<Traits> singletonEntityManager;
+	return &singletonEntityManager;
+}
+
+template<typename Traits>
+EntityID EntityManager<Traits>::newEntity()
+{
+	if (_freeList == nullptr)
+	{
+		// no invalid ID.
+		return 0;
+	}
+
+	EntityID retID = 0;
+
+	// if there is only one id left in the first block.
+	if (_freeList->start == _freeList->end)
+	{
+		retID = _freeList->start;
+		// delete the first block
+		// move the pointer to the next block.
+		PEntityFreeBlock deleteBlock = _freeList;
+		_freeList = _freeList->next;
+		delete deleteBlock;
+	}
+	else
+	{
+		// the first block have more than one entity id,
+		// return the start, and substract it by one.
+		retID = _freeList->start++;
+	}
+
+	++_usedID;
+	// clear the mask.
+	_maskPool[retID].reset();
+	return retID;
+}
+
+template<typename Traits>
+bool EntityManager<Traits>::destoryEntity(EntityID destoriedID)
+{
+	// the entityID shouldn't be zero.
+	ASSERT(destoriedID != 0);
+
+	PEntityFreeBlock prev(nullptr), curr(_freeList);
+	PEntityFreeBlock newBlock = nullptr;
+
+	while (curr)
+	{
+		// break the loop if the curr block is start
+		// after the destoriedID.
+		if (curr->start > destoriedID)
+		{
+			break;
+		}
+		else
+		{
+			prev = curr;
+			curr = curr->next;
+		}
+	}
+
+	unsigned char operationFlag = 0;
+	if (prev == nullptr)
+	{
+		operationFlag |= 1;
+	}
+	else if (prev->end >= destoriedID)
+	{
+		// this is a error,
+		// it means that the caller is trying to destory the id
+		// which is still in the freeBlock.
+		// More detail can be found in the 'EntityManager::isValid(EntityID)'
+		return false;
+	}
+	else if ((prev->end + 1) == destoriedID)
+	{
+		operationFlag |= 2;
+	}
+
+	if (curr == nullptr)
+	{
+		operationFlag |= 4;
+	}
+	else if ((curr->start - 1) == destoriedID)
+	{
+		operationFlag |= 8;
+	}
+
+
+	// comment notice
+	// /\:				nullptr
+	// -|-|:			the pointer point to some block
+	// pointerA \_/ destoriedID:  
+	//					there is a gap between pointerA's block and the destoriedID
+	// pointerA -> destoriedID:  
+	//					the pointerA's block concatenate the destoriedID
+	switch (operationFlag)
+	{
+		// prev /\  curr /\, empty list
+	case (1 | 4):
+		// prev /\ , curr-|-|, prev\_/ dest\_/ curr
+	case 1:
+		_freeList = new EntityFreeBlock();
+		_freeList->start = _freeList->end = destoriedID;
+		_freeList->next = curr;
+		break;
+
+		// prev /\  curr-|-|, prev\_/ dest<-curr
+	case (1 | 8):
+		// prev-|-|, curr-|-|, prev\_/ dest<-curr
+	case 8:
+		// add destoreidID to the curr block.
+		curr->start = destoriedID;
+		break;
+
+		// prev-|-|, curr /\, prev -> dest\_/ curr
+	case (4 | 2):
+		// prev-|-|, curr-|-|, prev -> dest\_/ curr
+	case 2:
+		prev->end = destoriedID;
+		break;
+
+		// prev-|-|, curr /\, prev\_/ dest\_/ curr
+	case 4:
+		// prev-|-|, curr-|-|, prev\_/ dest\_/ curr
+	case 0:
+		newBlock = new EntityFreeBlock();
+		newBlock->start = newBlock->end = destoriedID;
+		// concat the new block
+		prev->next = newBlock;
+		newBlock->next = curr;
+		break;
+
+		// prev-|-|, curr-|-|, prev->next<-curr
+	case (2 | 8):
+		prev->end = curr->end;
+		prev->next = curr->next;
+		delete curr;
+		break;
+
+	default:
+		ASSERT(false && "Unexpected situation");
+	}
+
+	--_usedID;
+	return true;
+}
+
+template<typename Traits>
+bool EntityManager<Traits>::isValid(EntityID checkID)
+{
+	// zero is alway the invalid ID.
+	if (!checkID)
+	{
+		return false;
+	}
+	PEntityFreeBlock prev(nullptr), curr(_freeList);
+
+	while (curr)
+	{
+		// break the loop if the curr block is start
+		// after the destoriedID.
+		if (curr->start > checkID)
+		{
+			break;
+		}
+		else
+		{
+			prev = curr;
+			curr = curr->next;
+		}
+	}
+
+	// the error Image:
+	//prev						curr
+	// |						/ \
+	// V					   /   \
+	//|-----------|			null OR	|-----------|
+	//|start   end|					|start   end|
+	//|-----------|					|-----------|
+	//	   /\   /|
+	//	   |OR /
+	//	   |  /
+	//	checkID
+
+	unsigned char operationFlag = 0;
+	if (prev != nullptr && (prev->end) >= checkID)
+	{
+		// this is a error,
+		// it means that the caller is trying to destory the id
+		// which is still in the freeBlock.
+		return false;
+	}
+
+	return true;
+}
+
+template<typename Traits>
+size_t EntityManager<Traits>::getSize() const
+{
+	return Traits::MaxEntityCount;
+}
+
+template<typename Traits>
+size_t EntityManager<Traits>::getUsedIDCount() const
+{
+	return _usedID;
+}
+
+template<typename Traits>
+EntityManager<Traits>::EntityManager()
+	: _usedID(0)
+{
+	static_assert(Traits::MaxEntityCount > 0);
+	_freeList = new EntityFreeBlock();
+	_freeList->start = 1;
+	_freeList->end = Traits::MaxEntityCount;
+	_freeList->next = nullptr;
+}
+
+template<typename Traits>
+EntityManager<Traits>::~EntityManager()
+{
 }
 
 }// namespace ECS
