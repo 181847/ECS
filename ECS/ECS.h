0ebccdb8f4fa01a63356e2083c155496e3034f09
181847 <753090084@qq.com>
2018-01-18 14:38:21 +0800

    将ECS.h中关于componentMask的定义和方便的函数转移到EntityManager中去。

--- ECS/ECS.h
+++ ECS/ECS.h
@@ -14,12 +14,6 @@ static const size_t gMaxComponentTypeCount	= 32;
 	// seral key type in the ECS.
 using EntityID			= size_t;
 using ComponentTypeID	= size_t;
-using ComponentMask		= std::bitset<gMaxComponentTypeCount>;
-
-template<typename ...COMPONENT_TYPES>
-ComponentMask getComponentMask();
-
-
 
 // this struct have no memeber, 
 // its main goal is to generat the unique number
@@ -32,15 +26,5 @@ int init();
 
 	// initialize the gECSLogger.
 int initLogger();
-
-template<typename ...COMPONENT_TYPES>
-inline ComponentMask getComponentMask()
-{
-	static ComponentMask mask(0);
-	static const bool args[] = { 
-		(false), // in case that the COMPONENT_TYPES is zero.
-		(mask |= 1ull << ComponentIDGenerator::IDOf<COMPONENT_TYPES>(), false)... };
-	return mask;
-}
 
 }// namespace ECS
