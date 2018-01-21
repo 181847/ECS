c65bd763d49382d82778899902479e0168ed794a
181847 <753090084@qq.com>
2018-01-21 22:59:27 +0800

    为ECSWorld添加函数以及访问安全控制。
    增加删除某个组件类型的函数。
    
    增加安全访问控制，防止在Foreach或者ForOne中执行对ECSWorld的任何操作，不允许出现访问某个Entity的时候把它删除掉。
    
    使用一个静态的变量s_isLocked进行控制，使用RAII的机制对s_isLocked进行设置，为此专门创建一个内部结构体StaticLockGuard，一次只允许一个代码执行到s_isLocked这里，否则将会触发断言。

--- ECS/ECSWorld.h
+++ ECS/ECSWorld.h
@@ -68,6 +68,25 @@ private:
 	static COMPONENT_TYPE* getComponent(ECS::EntityID id);
 
 	static ECS::EntityManager<EntityManagerTraits>* s_pEntityManager;
+
+	// Prevent from doing some work inside the Foreach or ForOne,
+	// such cannot Detach a component from the entity inside a Foreach operation.
+	// Every time we do some operation of the entityManager or the componentManager,
+	// we must ensure the s_isLocked is false, (if not, assert),
+	// then before we start the operation, set the s_isLocked to be true.
+	static bool s_isLocked;
+
+	// This struct used to manager the s_isLocked,
+	// when it is constructed, ensure the s_isLocked is false,(if not, assert),
+	// then set s_isLocked to be true, 
+	// when it is deconstructed, set the s_isLocked to te false.
+	// Prevent from doing some work inside the Foreach or ForOne,
+	// such cannot Detach a component from the entity inside a Foreach operation.
+	struct StaicLockGuard
+	{
+		StaicLockGuard();
+		~StaicLockGuard();
+	};
 };
 
 #pragma region ECSWorld static members definations
@@ -75,6 +94,9 @@ private:
 // Initialize static member.
 template<typename EntityManagerTraits, typename ...ComponentType>
 ECS::EntityManager<EntityManagerTraits>* ECSWorld<EntityManagerTraits, ComponentType...>::s_pEntityManager = ECS::EntityManager<EntityManagerTraits>::getInstance();
+
+template<typename EntityManagerTraits, typename ...ComponentType>
+bool ECSWorld<EntityManagerTraits, ComponentType...>::s_isLocked = false;
 
 #pragma endregion
 
@@ -86,6 +108,8 @@ inline void ECSWorld<EntityManagerTraits, ComponentType...>::Foreach(std::functi
 {
 	static_assert(CheckTypeSupported<COMPONENT_TYPE_LIST...>::value,
 		"Exist not supported component type");
+
+	StaicLockGuard avoidConfliction;
 
 	for (EntityID  id : s_pEntityManager->RangeEntities<COMPONENT_TYPE_LIST...>())
 	{
@@ -100,6 +124,8 @@ inline bool ECSWorld<EntityManagerTraits, ComponentType...>::ForOne(EntityID id,
 	static_assert(CheckTypeSupported<COMPONENT_TYPE_LIST...>::value,
 		"Exist not supported component type");
 
+	StaicLockGuard avoidConfliction;
+
 	if ( ! s_pEntityManager->isValid(id))
 	{
 		// The id is wrong.
@@ -113,19 +139,60 @@ inline bool ECSWorld<EntityManagerTraits, ComponentType...>::ForOne(EntityID id,
 
 template<typename EntityManagerTraits, typename ...ComponentType>
 template<typename COMPONENT_TYPE, typename ...COMPONENT_CONSTRUCT_ARGS>
-inline bool ECSWorld<EntityManagerTraits, ComponentType...>::
-AttachTo(EntityID targetID, COMPONENT_CONSTRUCT_ARGS && ...args)
+inline bool ECSWorld<EntityManagerTraits, ComponentType...>::AttachTo(EntityID targetID, COMPONENT_CONSTRUCT_ARGS && ...args)
 {
 	static_assert(CheckTypeSupported<COMPONENT_TYPE>::value,
 		"Not supported component type");
 
-	if ( ! s_pEntityManager->isValid(targetID))
+	StaicLockGuard avoidConfliction;
+
+	assert(s_pEntityManager->isValid(targetID) && 
+		"The entity you are operated on is invalid.");
+
+	// Dose the entity already have the component?
+	if (s_pEntityManager->haveComponent<COMPONENT_TYPE>(targetID))
+	{
+		// Yes, it has. Just return false tell the caller the operation failed.
 		return false;
+	}
 
 	COMPONENT_TYPE * pNewComponent = 
 		ECSWorldAlias::getComponentManager<COMPONENT_TYPE>()
 		->newComponnet<COMPONENT_CONSTRUCT_ARGS...>(targetID, std::forward<COMPONENT_CONSTRUCT_ARGS>(args)...);
 	s_pEntityManager->maskComponentType<COMPONENT_TYPE>(targetID);
+
+	return true;
+}
+
+template<typename EntityManagerTraits, typename ...ComponentType>
+template<typename ...COMPONENT_TYPE_LIST>
+inline bool ECSWorld<EntityManagerTraits, ComponentType...>::DetachFrom(EntityID targetID)
+{
+	static_assert(CheckTypeSupported<COMPONENT_TYPE_LIST...>::value,
+		"Not supported component type");
+
+	StaicLockGuard avoidConfliction;
+
+	assert(s_pEntityManager->isValid(targetID) &&
+		"The entity you are operated on is invalid.");
+
+	// Dose the entity have all the component type?
+	if ( ! s_pEntityManager->haveComponent<COMPONENT_TYPE_LIST...>(targetID))
+	{
+		// No, the entity may lack some component type, or maybe it dosen't have any one.
+		return false;// operation failed.
+	}
+
+	// unpack operation for each component type.
+	bool arrayInit[] =
+	{
+		(
+			getComponentManager<COMPONENT_TYPE_LIST>()->removeComponent(targetID)
+		)...
+	};
+
+	s_pEntityManager->removeMask<COMPONENT_TYPE_LIST...>(targetID);
+
 	return true;
 }
 
@@ -164,9 +231,26 @@ inline COMPONENT_TYPE * ECSWorld<EntityManagerTraits, ComponentType...>::getComp
 template<typename EntityManagerTraits, typename ...ComponentType>
 inline EntityID ECSWorld<EntityManagerTraits, ComponentType...>::NewEntity()
 {
+	StaicLockGuard avoidConfliction;
+
 	return s_pEntityManager->newEntity();
 }
 
 #pragma endregion
+
+template<typename EntityManagerTraits, typename ...ComponentType>
+inline ECSWorld<EntityManagerTraits, ComponentType...>::StaicLockGuard::StaicLockGuard()
+{
+	assert(ECSWorldAlias::s_isLocked == false && "Confiction happend, other operation is accessing the ECSWorld.");
+	// set lock.
+	s_isLocked = true;
+}
+
+template<typename EntityManagerTraits, typename ...ComponentType>
+inline ECSWorld<EntityManagerTraits, ComponentType...>::StaicLockGuard::~StaicLockGuard()
+{
+	// free the lock.
+	ECSWorldAlias::s_isLocked = false;
+}
 
 }// namespace ECS
